24b567326af6bc4a73cb70846a09b992399287d1
alejolo311 <alejolo311@gmail.com>
2020-12-12 11:29:47 -0500

    Fix Style
    Update the code style using google c++ styling guide

--- 0x00 - intro C++/0x00.cpp
+++ 0x00 - intro C++/0x00.cpp
@@ -1,25 +1,28 @@
+/* Copyright 2020 <alejolo311> */
+
 /* Function overloading */
 
 #include <iostream>
 #include "intro.hpp"
 
 
-void toa(int value){
+void toa(int32_t value) {
     std::cout << value << " is a int" << std::endl;
 }
-void toa(long value){
-    std::cout << value << " is a long" << std::endl;
+void toa(int16_t value) {
+    std::cout << value << " is a short" << std::endl;
 }
-void toa(unsigned long value){
-    std::cout << value << " is a unsigned long" << std::endl;
+void toa(uint16_t value) {
+    std::cout << value << " is a unsigned short" << std::endl;
 }
 
 
 int main() {
+    int32_t a = 1;
+    int16_t b = 2;
+    uint16_t c = 3;
 
-    int a = 1; 
-    long b = 2; 
-    unsigned long c = 3; 
+
     toa(a);
     toa(b);
     toa(c);
